24d2829ea2e53e6b714fa1028e3ef4746f835da9
SarinaSevda <sarina.schoki@gmail.com>
2025-02-04 21:33:53 +0100

    added basic styling to main.py

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ def submit_name():
     else:
         messagebox.showwarning("Warnung", "Bitte gib deinen Namen ein.")
 
+def ask_addiction():
+    welcome_label.config(text=f"Willkommen, {user_data['name']}!", bg="white")
+    welcome_label.pack(pady=50)
+    addiction_label.pack()
+    addiction_dropdown.pack(pady=10)
+    addiction_button.pack()
+
 def submit_addiction():
     user_data["addiction"] = addiction_var.get()
     if user_data["addiction"] != "Bitte auswählen":
@@ -31,12 +38,13 @@ def submit_addiction():
     else:
         messagebox.showwarning("Warnung", "Bitte wähle eine Sucht aus.")
 
-def ask_addiction():
+
+def ask_goal():
     welcome_label.config(text=f"Willkommen, {user_data['name']}!")
     welcome_label.pack()
-    addiction_label.pack()
-    addiction_dropdown.pack()
-    addiction_button.pack()
+    goal_label.pack()
+    goal_dropdown.pack(pady=10)
+    goal_button.pack()
 
 def submit_goal():
     user_data["goal"] = goal_var.get()
@@ -50,12 +58,14 @@ def submit_goal():
     else:
         messagebox.showwarning("Warnung"), "Bitte wähle ein Ziel aus."
 
-def ask_goal():
-    welcome_label.config(text=f"Willkommen, {user_data['name']}!")
-    welcome_label.pack()
-    goal_label.pack()
-    goal_dropdown.pack()
-    goal_button.pack()
+
+def ask_sobriety_duration():
+    sobriety_label.pack()
+    sobriety_date.pack(pady=10)
+    sobriety_time_label.pack(pady=10)
+    sobriety_hour_dropdown.pack()
+    sobriety_minute_dropdown.pack()
+    sobriety_button.pack()
 
 def submit_sobriety_duration():
     raw_date = sobriety_date.get()
@@ -75,26 +85,16 @@ def submit_sobriety_duration():
     else:
         messagebox.showwarning("Warnung", "Bitte gib ein Datum und eine Zeit an.")
 
-def ask_sobriety_duration():
-    sobriety_label.pack()
-    sobriety_date.pack()
-    sobriety_time_label.pack()
-    sobriety_hour_dropdown.pack()
-    sobriety_minute_dropdown.pack()
-    sobriety_button.pack()
 
 def ask_final_welcome():
     final_welcome_label.pack()
     final_welcome_text.pack()
     final_welcome_button.pack()
 
-
-
 def open_main_window():
     """Öffnet das Hauptfenster der Anwendung."""
     root.destroy()
     main_window(user_data)
-
 
 
 
@@ -106,50 +106,53 @@ root.configure(bg="white")
 root.resizable(False, False)
 
 # Erste Abfrage: Wie heißt du?
-welcome_label = tk.Label(root, text="Willkommen")
-name_label = tk.Label(root, text="Wie heißt du?")
-name_label.pack()
+welcome_label = tk.Label(root, text="Willkommen!", font=("Helvetica", 20, "bold"), bg="white")
+name_label = tk.Label(root, text="Wie ist dein Name?", font=("Helvetica", 20, "bold"), bg="white")
+name_label.pack(pady=50)
 
 name_entry = tk.Entry(root)
-name_entry.pack()
+name_entry.pack(pady=10)
 
 name_button = tk.Button(root, text="Weiter", command=submit_name)
 name_button.pack()
 
 # Zweite Abfrage: Sucht (initial unsichtbar)
-welcome_label = tk.Label(root, text="", font=("Helvetica", 12, "bold"))
-addiction_label = tk.Label(root, text="Wähle deine Sucht aus")
+welcome_label = tk.Label(root, text="", font=("Helvetica", 20, "bold"))
+addiction_label = tk.Label(root, text="Wähle deine Sucht aus:", font=("Helvetica", 13), bg="white")
 addiction_var = tk.StringVar(value="Bitte auswählen")  # Standardwert für das Dropdown
-addiction_options = ["Bitte auswählen", "Alkohol", "Zigaretten", "Tierische Produkte", "Drogen", "Kaffee", "Schokolade", "Videospiele"]
+addiction_options = ["Bitte auswählen", "Alkoholsucht", "Nikotinsucht", "Drogensucht", "Internetsucht", "Tablettensucht", "Spielsucht", "Kaufsucht", "Tierische Produkte", "Koffeinsucht", "Andere"]
 addiction_dropdown = tk.OptionMenu(root, addiction_var, *addiction_options)
 addiction_button = tk.Button(root, text="Weiter", command=submit_addiction)
 
 
 #Dritte Abfrage: Ziele
-welcome_label = tk.Label(root, text="", font=("Helvetica", 12, "bold"))
-goal_label = tk.Label(root, text="Was möchtest du erreichen?")
+welcome_label = tk.Label(root, text="", font=("Helvetica", 20, "bold"))
+goal_label = tk.Label(root, text="Was möchtest du erreichen?", font=("Helvetica", 13), bg="white")
 goal_var = tk.StringVar(value="Bitte auswählen")
-goal_options = ["Weiger Alkohol trinken", "Gesunde Gewohnheiten aufbauen", "Rauchen aufgeben", "Nüchtern bleiben", "Bildschirmzeit reduzieren"]
+goal_options = ["Weniger Alkohol trinken", "Gesunde Gewohnheiten etablieren", "Rauchen aufgeben", "Nüchtern bleiben", "Bildschirmzeit reduzieren", "Geld sparen", "Mehr Fokus finden", "Andere"]
 goal_dropdown = tk.OptionMenu(root, goal_var, *goal_options)
 goal_button = tk.Button(root, text="Weiter", command=submit_goal)
 
 #Vierte Abfrage
-sobriety_label = tk.Label(root, text="Seit wann bist du nüchtern?")
-sobriety_date = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)
+sobriety_label = tk.Label(root, text="Seit wann bist du nüchtern?", font=("Helvetica", 13), bg="white")
+sobriety_date = DateEntry(root, width=12, borderwidth=2)
 
-sobriety_time_label = tk.Label(root, text="Um welche Uhrzeit?")
+sobriety_time_label = tk.Label(root, text="Um welche Uhrzeit?", font=("Helvetica", 13), bg="white")
+
 sobriety_hour = tk.StringVar(value="00")
-sobriety_hour_dropdown = tk.OptionMenu(root, sobriety_hour, *[f"{i:02}" for i in range(24)])  # Stunden 00-23
+sobriety_hour_dropdown = tk.OptionMenu(root, sobriety_hour, *[f"{i:02}" for i in range(24)])
+
 sobriety_minute = tk.StringVar(value="00")
-sobriety_minute_dropdown = tk.OptionMenu(root, sobriety_minute, *[f"{i:02}" for i in range(60)])  # Minuten 00-59
+sobriety_minute_dropdown = tk.OptionMenu(root, sobriety_minute, *[f"{i:02}" for i in range(60)])
+
+
 sobriety_button = tk.Button(root, text="Weiter", command=submit_sobriety_duration)
 
 
 # Fünfte "Abfrage", eher das finale Welcome Window:
-final_welcome_label = tk.Label(root, text="Willkommen auf deiner Sobriety-Reise", font=("Helvetica", 12, "bold"))
-final_welcome_text = tk.Label(root, text="Im nächsten Schritt zeigen wir dir deinen täglichen Tracker, in dem du deine nüchternen tage verfolgen und dokumentieren kannst und in dem wir dir tägliche Hilfestellungen zur Verfügung stellen.")
+final_welcome_label = tk.Label(root, text="Willkommen auf deiner Sobriety-Reise", font=("Helvetica", 13, "bold"), bg="white")
+final_welcome_text = tk.Label(root, text="Im nächsten Schritt zeigen wir dir deinen täglichen Tracker, in dem du deine nüchternen tage verfolgen und dokumentieren kannst und in dem wir dir tägliche Hilfestellungen zur Verfügung stellen.", font=("Helvetica", 13), wraplength=400, bg="white")
 final_welcome_button = tk.Button(root, text="Weiter", command=open_main_window)
-
 
 
 root.mainloop()
